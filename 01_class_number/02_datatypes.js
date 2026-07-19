b1bfaa284b1cfaefa448d843fc2e3e4205218673
HARIT PAL <haritpal955753@outlook.com>
2026-07-19 13:49:45 +0000

    02 Data types

---
+++ 01_class_number/02_datatypes.js
@@ -0,0 +1,37 @@
+"use struct"; // treat all JS code an newer version
+
+// alert( 3 + 3 ) // we are using nodejs not browser
+
+console.log( 3+3 ) // code readability should be high 
+
+console.log("HARIT PAL")
+
+
+
+let name = "HARIT PAL"
+let age = 26
+let isLoggedIn = false
+
+let State;
+
+
+
+
+               // number => 2 to power 53
+                //  bigint
+                //string => ""
+                //Boolean => true/false
+                //null => standalone value
+                //undefined =>
+                    //symbol =unique
+                //
+
+
+                    // object
+
+
+
+
+                    console.log(typeof "HARIT PAL");
+                    console.log(typeof age);
+                    
