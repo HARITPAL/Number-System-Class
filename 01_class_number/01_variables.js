28065e2002d58bba5b228ee8b374aed47cb0f308
HARIT PAL <haritpal955753@outlook.com>
2026-07-19 06:43:55 +0000

    01_variables.js

---
+++ 01_class_number/01_variables.js
@@ -0,0 +1,14 @@
+const accountID = 14314382
+let accountEmail = "haritpal955753@gmail.com"
+var accountPassword = "114433"
+accountCity = "Jaipur"
+
+// accountID = 8 // not allowed
+
+accountEmail = "haritpal143@gmail.com"
+accountPassword = "143143143"
+accountCity = "Muzaffarnagar"
+
+console.log(accountID);
+
+console.table([accountID, accountEmail, accountPassword, accountCity]); 
